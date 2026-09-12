126fea5ea5f51e0faf511ca569650636b0484696
Gabriel Oliva <98856055+SkullBBoy@users.noreply.github.com>
2026-09-12 16:44:56 -0300

    base switch
    .

--- TP-AyED-HTML---UTN-FRBA/main.cpp
+++ TP-AyED-HTML---UTN-FRBA/main.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
+#include <windows.h>
+#include <locale.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+
+
+int main(){
+
+    int opcion;
+
+
+    do{
+    system("cls");
+
+    cout<<"1. Cargar archivo de ataque en memoria."<<endl;
+    cout<<"2. Mostrar ataque cargado."<<endl;
+    cout<<"3. Crear un archivo de ataque nuevo."<<endl;
+    cout<<"4. Corregir un registro del archivo. "<<endl;
+    cout<<"5. Corregir un registro en memoria."<<endl;
+    cout<<"6. Guardar memoria en un archivo nuevo."<<endl;
+    cout<<"7. Visualizar un archivo de ataque en html. "<<endl;
+
+    cout<<endl<<"Opcion: "<<endl;
+    cin>>opcion;
+
+
+    switch(opcion){
+
+        case 1:
+
+
+        break;
+        case 2:
+
+
+        break;
+        case 3:
+
+
+        break;
+        case 4:
+
+
+        break;
+        case 5:
+
+
+        break;
+        case 6:
+
+
+        break;
+
+        case 7:
+
+
+        break;
+
+        default:
+            if(opcion==0 || opcion==8){
+                system("cls");
+                cout<<"Fin del programa"<<endl;
+
+            }else{
+                system("cls");
+                cout<<"Opcion invalida"<<endl;
+                system("pause");
+            }
+
+    }}while(opcion!=0 && opcion !=8);
+
+
     return 0;
 }
